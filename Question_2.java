7a37b7b196611e51904b4434d9ce0d7ed6898903
Porokh2002 <porokhjyoti198@gmail.com>
2023-02-11 15:22:55 +0530

    2nd Question

---
+++ Question_2.java
@@ -0,0 +1,8 @@
+//Write a program to reverse a String
+public class Question_2{
+    public static void main(String[] args) {
+        StringBuffer str= new StringBuffer("PWSKILLS");
+        str.reverse();
+        System.out.println(str);
+    }
+}
